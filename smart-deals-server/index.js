3af19b1dcd34349b53b9c7a245bfe475cf31a530
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-11-06 20:31:28 +0600

    APIs ared Created to perform CRUD operations

--- smart-deals-server/index.js
+++ smart-deals-server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000
 
@@ -7,10 +8,136 @@ const port = process.env.PORT || 3000
 app.use(cors());
 app.use(express.json());
 
+// GET API
 app.get('/', (req, res) => {
     res.send('Hello from server');
 });
 
+// connection uri
+const uri = "mongodb+srv://smartdbUser:3iiFOE0iiWpG8vOI@cluster0.edix7i0.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        // Create DataBase
+        const db = client.db('smart_db');
+
+        // Create Collections
+        const productCollection = db.collection('products');
+        const bidsCollection = db.collection('bids');
+
+        // GET API to get all the products data
+        app.get('/products', async (req, res) => {
+            // const sortWithDescendingPrice = { price_min: -1 };
+            // const sortWithAscendingPrice = { price_min: 1 };
+            // const cursor = productCollection.find().sort(sortWithDescendingPrice);
+
+            // const cursor = productCollection.find().limit(2);
+            // const cursor = productCollection.find().skip(2).limit(2);
+            // const cursor = productCollection.find().project({title: 1, _id: 0});//only title will shown
+
+            // Find Data with Query Parameter
+            // const email = req.query.email;
+            // const query = {};
+            // if (email) {
+            //     query.email = email;
+            // }
+            // const cursor = productCollection.find(query);
+
+            const cursor = productCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        // GET API to find specific product data
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await productCollection.findOne(query);
+            res.send(result);
+        });
+
+        // POST API to create a product
+        app.post('/products', async (req, res) => {
+            const newProduct = req.body;
+            const result = await productCollection.insertOne(newProduct);
+            res.send(result);
+        });
+
+        // PATCH API to update a product details
+        app.patch('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const update = {
+                $set: {
+                    name: req.body.name,
+                    email: req.body.name
+                }
+            };
+            const options = {};
+            const result = await productCollection.updateOne(query, update, options);
+            res.send(result);
+        });
+
+        // DELETE API to delete a product
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = productCollection.deleteOne(query);
+            res.send(result);
+        });
+
+        // bids related APIs
+        // GET API to get the bids
+        app.get('/bids', async (req, res) => {
+            const email = req.query.email;
+            const query = {};
+            if (email) {
+                query.buyer_email = email;
+            }
+
+            const cursor = bidsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        // POST API: to post a bids
+        app.post('/bids', async (req, res) => {
+            const newBids = req.body;
+            const result = await bidsCollection.insertOne(newBids);
+            res.send(result);
+        });
+
+        // DELETE API: to remove bids
+        app.delete('/bids/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await bidsCollection.deleteOne(query);
+            res.send(result);
+        });
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+// Listen the Server
 app.listen(port, (req, res) => {
     console.log(`Server is running at: http://localhost:${port}`);
 });
