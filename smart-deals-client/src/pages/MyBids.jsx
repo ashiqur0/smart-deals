f39c5d1aaaaf7fbedb73800bb2590c1e099aa90f
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-11-07 19:18:00 +0600

    Bid Delete Functionality Added

--- smart-deals-client/src/pages/MyBids.jsx
+++ smart-deals-client/src/pages/MyBids.jsx
@@ -3,6 +3,7 @@ import { use } from 'react';
 import { useEffect } from 'react';
 import { AuthContext } from '../context/AuthContext';
 import { useState } from 'react';
+import Swal from 'sweetalert2';
 
 const MyBids = () => {
     const { user } = use(AuthContext);
@@ -17,7 +18,41 @@ const MyBids = () => {
                     setBids(data);
                 })
         }
-    }, [user?.email])
+    }, [user?.email]);
+
+    const handleDeleteBid = (_id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                fetch(`http://localhost:3000/bids/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log('after delete', data);
+                        if (data.deletedCount) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your bid has been deleted.",
+                                icon: "success"
+                            });
+
+                            // update the state with remaining bids
+                            const remainingBids = bids.filter(bid => bid._id !== _id);
+                            setBids(remainingBids);
+                        }
+                    })
+            }
+        });
+    }
 
     return (
         <div>
@@ -32,7 +67,8 @@ const MyBids = () => {
                             <th>Buyer Name</th>
                             <th>Buyer Email</th>
                             <th>Bid Price</th>
-                            <th>Details</th>
+                            <th>Status</th>
+                            <th>Actions</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -55,9 +91,11 @@ const MyBids = () => {
                                 </td>
                                 <td>{bid.buyer_email}</td>
                                 <td>{bid.bid_price}</td>
-                                <th>
-                                    <button className="btn btn-ghost btn-xs">details</button>
-                                </th>
+                                <th><div className={`badge badge-${bid.status === 'pending' ? 'warning' : 'success'}`}>{bid.status}</div></th>
+                                <td><button
+                                    onClick={() => handleDeleteBid(bid._id)}
+                                    className='btn btn-outline btn-xs'
+                                >Remove Bid</button></td>
                             </tr>)
                         }
                     </tbody>
